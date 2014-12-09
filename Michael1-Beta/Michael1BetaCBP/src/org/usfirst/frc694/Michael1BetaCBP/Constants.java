224fceb64911c8a90e86a7a60aa881ffbca8f3f1
Danny Qiu <dqiu55@gmail.com>
2014-12-09 00:33:07 -0500

    Regenerate project through RobotBuilder update

---
+++ Michael1-Beta/Michael1BetaCBP/src/org/usfirst/frc694/Michael1BetaCBP/Constants.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc694.Michael1BetaCBP;
+
+public class Constants {
+
+	/* USB PORTS */
+	static final int LEFT_STICK_PORT = 0;
+	static final int RIGHT_STICK_PORT = 1;
+	
+	/* PWM OUT */
+	static final int DRIVETRAIN_LEFT_CHANNEL = 0;
+	static final int DRIVETRAIN_RIGHT_CHANNEL = 1;
+	static final int ACQUIRER_TOP_ROLLER = 2;
+	static final int ACQUIRER_BOTTOM_ROLLER = 3;
+	static final int PADDLE_SERVO_CHANNEL = 4;
+
+	/* RELAYS */
+	static final int SHOOTER_CHANNEL = 0;
+	
+}
